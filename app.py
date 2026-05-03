331b8d9093d1c20b0732d5d49f24a3039c4085d6
Emmanuel-18 <emmanuel.generale118@gmail.com>
2026-05-03 23:05:50 +0800

    Traing and publish it as service using fastAPI

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 print("Hello from GitHub Jenkins integration")
 
+from pydantic import BaseModel
+from typing import List
 from fastapi import FastAPI
 import joblib
 import numpy as np
@@ -8,12 +10,17 @@ app = FastAPI()
 
 model = joblib.load("best_model.pkl")
 
+class InputData(BaseModel):
+    features: List[float]
+
+    
+
 @app.get("/")
 def home():
     return {"message": "ML Model API is running"}
 
 @app.post("/predict")
-def predict(features: list):
-    data = np.array(features).reshape(1, -1)
+def predict(input_data: InputData):
+    data = np.array(input_data.features).reshape(1, -1)
     prediction = model.predict(data)
-    return {"prediction": int(prediction[0])}
+    return {"prediction": prediction.tolist()}
